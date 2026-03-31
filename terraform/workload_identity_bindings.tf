47e15df97ffea621c81c878fdbb3f566b8cdba14
Shivam Sharma <shivamsharma.ade@gmail.com>
2026-03-31 17:03:05 +0530

    update services to use a service account

---
+++ terraform/workload_identity_bindings.tf
@@ -0,0 +1,40 @@
+locals {
+  workload_pool = "${var.project_id}.svc.id.goog"
+  workload_ns   = "mcart"
+}
+
+# Allow Kubernetes service accounts to impersonate the corresponding GCP service accounts.
+# Required for Workload Identity (without this, pods get PERMISSION_DENIED to GCP APIs).
+
+resource "google_service_account_iam_member" "wi_auth" {
+  count = local.auth_sa != "" && var.enable_workload_identity ? 1 : 0
+
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.auth_sa}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.workload_pool}[${local.workload_ns}/auth]"
+}
+
+resource "google_service_account_iam_member" "wi_user" {
+  count = local.user_sa != "" && var.enable_workload_identity ? 1 : 0
+
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.user_sa}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.workload_pool}[${local.workload_ns}/user]"
+}
+
+resource "google_service_account_iam_member" "wi_product" {
+  count = local.product_sa != "" && var.enable_workload_identity ? 1 : 0
+
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.product_sa}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.workload_pool}[${local.workload_ns}/product]"
+}
+
+resource "google_service_account_iam_member" "wi_product_indexer" {
+  count = local.product_indexer_sa != "" && var.enable_workload_identity ? 1 : 0
+
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${local.product_indexer_sa}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${local.workload_pool}[${local.workload_ns}/product-indexer]"
+}
+
